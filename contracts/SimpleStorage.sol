0a4c522ab6138a9ba5e295a7cd03af3f224b8d53
"ssghost" <"ssprof0@gmail.com">
2022-03-19 23:53:39 +0800

    update types

--- contracts/SimpleStorage.sol
+++ contracts/SimpleStorage.sol
@@ -1,1 +1,10 @@
-pragma solidity >=0.6.0 <0.9.0;
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    uint256 myNumber = 1024;
+    int256 myNegNumber = -1024;
+    bool myBool = true;
+    string myString = "Hello, world.";
+    address myAddress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
+    bytes32 myBytes = "eth";
+}
