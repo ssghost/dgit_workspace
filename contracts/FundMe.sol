bcab5b6bf201ab0c1935f79b50a231d5a145c85b
"ssghost" <"ssprof0@gmail.com">
2022-03-22 20:46:23 +0800

    update datafeed

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -13,12 +13,7 @@ contract FundMe {
         priceFeed = AggregatorV3Interface(0x9326BFA02ADD2366b30bacB125260Af641031331);
     }
     function getLatestPrice() public view returns (int) {
-        (uint80 roundID,
-            int price,
-            uint startedAt,
-            uint timeStamp,
-            uint80 answeredInRound
-        ) = priceFeed.latestRoundData();
+        (,int price,,,) = priceFeed.latestRoundData();
         return price;
     }
     function getVersion() public view returns (uint256) {
