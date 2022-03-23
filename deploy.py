02e657d3d45877eaafe9bd9580ca569d845c509e
ssghost_gk <shuangsong00@outlook.com>
2022-03-24 00:10:36 +0800

    update web3 deployment

--- deploy.py
+++ deploy.py
@@ -1,4 +1,5 @@
 from solc import compile_standard
+from web3 import Web3
 import json
 
 with open("./contracts/SimpleStorage.sol",'r') as f:
@@ -13,5 +14,12 @@ compiled_file = compile_standard({
 with open("compiled.json",'w') as f:
     json.dump(compiled_file, f)
 
-bytecode= compiled_file["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"]["bytecode"]["object"]
-abi= compiled_file["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+bytecode = compiled_file["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"]["bytecode"]["object"]
+abi = compiled_file["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
+
+myweb3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+chain_id = 5777
+myaddress = "0xC14605A85b76092B3D0b8B6c1Acc46F1F4029C17"
+pri_key = "fe041f36a5e19a764788eecc7f071cbea3a89f650e961810a07ea4a4222477f9"
+
+myContract = Web3.eth.contract(bytecode=bytecode, abi=abi)
