7d0e7bd1f212e4f5e200be5cfa3da62831d1f164
ssghost <ssprof0@gmail.com>
2022-03-24 19:27:51 +0800

    brownie deploy c1

--- brownie-test/scripts/deploy.py
+++ brownie-test/scripts/deploy.py
@@ -1,7 +1,12 @@
-from brownie import accounts
+from brownie import accounts, config, SimpleStorage
 
 def deploy_c1():
-    account = accounts.load("test_01")
-
+    account = accounts.add(config['wallets']['from_key'])
+    dContract1 = SimpleStorage.deploy({'from': account})
+    print(dContract1.retrieveStorage())
+    Txn1 = dContract1.updateStorage(42, {'from':account})
+    Txn1.wait(1)
+    print(dContract1.retrieveStorage())
+    
 if __name__=='__main__':
     deploy_c1()
